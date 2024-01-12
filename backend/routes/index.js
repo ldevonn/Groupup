75047dc1b82c17c98ac8a8ef89d4f602ffe90f19
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-01-11 22:20:32 -0700

    added frontend start

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,12 +5,34 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
-  });
-});
+if (process.env.NODE_ENV === "production") {
+	const path = require("path");
+	router.get("/", (req, res) => {
+		res.cookie("XSRF-TOKEN", req.csrfToken());
+		return res.sendFile(
+			path.resolve(__dirname, "../../frontend", "dist", "index.html")
+		);
+	});
+
+	router.use(express.static(path.resolve("../frontend/build")));
+
+	router.get(/^(?!\/?api).*/, (req, res) => {
+		res.cookie("XSRF-TOKEN", req.csrfToken());
+		return res.sendFile(
+			path.resolve(__dirname, "../../frontend", "build", "index.html")
+		);
+	});
+}
+
+if (process.env.NODE_ENV !== "production") {
+	router.get("/api/csrf/restore", (req, res) => {
+		res.cookie("XSRF-TOKEN", req.csrfToken());
+		return res.json({});
+	});
+}
+
+// router.post("/api/test", function (req, res) {
+// 	res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
