d736408b8496e4661bd76626bbd20bcde0c76f5e
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-01-16 15:18:48 -0700

    deploy build

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,30 +9,29 @@ if (process.env.NODE_ENV === "production") {
 	const path = require("path");
 	router.get("/", (req, res) => {
 		res.cookie("XSRF-TOKEN", req.csrfToken());
-		return res.sendFile(
+		res.sendFile(
 			path.resolve(__dirname, "../../frontend", "dist", "index.html")
 		);
 	});
 
-	router.use(express.static(path.resolve("../frontend/build")));
+	router.use(express.static(path.resolve("../frontend/dist")));
 
 	router.get(/^(?!\/?api).*/, (req, res) => {
 		res.cookie("XSRF-TOKEN", req.csrfToken());
-		return res.sendFile(
-			path.resolve(__dirname, "../../frontend", "build", "index.html")
+		res.sendFile(
+			path.resolve(__dirname, "../../frontend", "dist", "index.html")
 		);
 	});
 }
 
 if (process.env.NODE_ENV !== "production") {
 	router.get("/api/csrf/restore", (req, res) => {
-		res.cookie("XSRF-TOKEN", req.csrfToken());
-		return res.json({});
+		const csrfToken = req.csrfToken();
+		res.cookie("XSRF-TOKEN", csrfToken);
+		res.status(200).json({
+			"XSRF-Token": csrfToken,
+		});
 	});
 }
-
-// router.post("/api/test", function (req, res) {
-// 	res.json({ requestBody: req.body });
-// });
 
 module.exports = router;
--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import {login} from '../../store/session.js'
 import {useModal} from '../../context/Modal.jsx'
 
