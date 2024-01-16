e96c27ddec933ce8ed91c033482643e51c158152
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-01-16 12:01:35 -0700

    modal setup complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store/store.js';
 import { restoreCSRF, csrfFetch } from './store/csrf.js';
 import * as sessionActions from './store/session.js'
+import {Modal, ModalProvider} from './context/Modal.jsx'
 
 const store = configureStore()
 if (process.env.NODE_ENV !== 'production'){
@@ -23,8 +24,11 @@ if (import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <ModalProvider>
         <Provider store={store}>
             <App />
+            <Modal />
         </Provider>
+        </ModalProvider>
     </React.StrictMode>
 );
